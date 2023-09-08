6ba3c108901845bf3ca7db7eb1d59b185e5385f1
JUan Andrade <jfandradea@gmail.com>
2023-09-07 20:12:01 -0500

    finish implementations

--- src/components/svelte/CloseBtn.svelte
+++ src/components/svelte/CloseBtn.svelte
@@ -1,41 +1,41 @@
 <script>
-  import { createEventDispatcher } from "svelte";
-  const dispatch = createEventDispatcher();
-  function toggleNav() {
-    dispatch("toggleNav");
-  }
-  export let navIsOpen;
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	function toggleNav() {
+		dispatch('toggleNav');
+	}
+	export let navIsOpen;
 </script>
 
 <button class:navIsOpen on:click={toggleNav}>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-    class="icon-sm"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-    />
-  </svg>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="icon-sm"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+		/>
+	</svg>
 </button>
 
 <style>
-  button {
-    display: none;
-    align-items: center;
-    padding: 0;
-    border: none;
-    cursor: pointer;
-    background: transparent;
-    color: white;
-  }
+	button {
+		display: none;
+		align-items: center;
+		padding: 0;
+		border: none;
+		cursor: pointer;
+		background: transparent;
+		color: white;
+	}
 
-  .navIsOpen {
-    display: flex;
-  }
+	.navIsOpen {
+		display: flex;
+	}
 </style>
--- src/components/svelte/NavBar.svelte
+++ src/components/svelte/NavBar.svelte
@@ -1,73 +1,65 @@
 <script>
-  import MobileBtn from "./MobileBtn.svelte";
-  import CloseBtn from "./CloseBtn.svelte";
-  let isOpen = false;
+	import MobileBtn from './MobileBtn.svelte';
+	import CloseBtn from './CloseBtn.svelte';
+	import Menu from './Menu.svelte';
+	let isOpen = false;
 
-  function toggleNav() {
-    isOpen = !isOpen;
-  }
+	function toggleNav() {
+		isOpen = !isOpen;
+	}
 </script>
 
 <div class="mobile-buttons">
-  <MobileBtn navIsOpen={isOpen} on:toggleNav={toggleNav} />
-  <CloseBtn navIsOpen={isOpen} on:toggleNav={toggleNav} />
+	<MobileBtn navIsOpen={isOpen} on:toggleNav={toggleNav} />
+	<CloseBtn navIsOpen={isOpen} on:toggleNav={toggleNav} />
 </div>
 
 <nav class="full-width" class:isOpen>
-  <ul
-    class="flex flex-col full-width gap items-center justify-center text-sm"
-    style="--gap-size: 1rem; "
-  >
-    <li>About</li>
-    <li>Careers</li>
-    <li>Events</li>
-    <li>Products</li>
-    <li>Support</li>
-  </ul>
+	<Menu />
 </nav>
 
 <style>
-  nav {
-    display: none;
-    color: white;
-    border: 1px solid hsl(var(--crl-white));
-  }
+	nav {
+		display: none;
+		color: white;
+		border: 1px solid hsl(var(--crl-white));
+	}
 
-  ul li {
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 1rem 0;
-    background-color: transparent;
-    color: hsl(var(--crl-white));
-  }
+	ul li {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		padding: 1rem 0;
+		background-color: transparent;
+		color: hsl(var(--crl-white));
+	}
 
-  .isOpen {
-    display: block;
-  }
+	.isOpen {
+		display: block;
+	}
 
-  @media screen and (min-width: 850px) {
-    nav {
-      display: block;
-    }
-    .mobile-buttons {
-      display: none;
-    }
-    ul {
-      flex-direction: row;
-      justify-content: flex-end;
-    }
-    ul li {
-      width: auto;
-      background-color: none;
-    }
+	@media screen and (min-width: 850px) {
+		nav {
+			display: block;
+		}
+		.mobile-buttons {
+			display: none;
+		}
+		ul {
+			flex-direction: row;
+			justify-content: flex-end;
+		}
+		ul li {
+			width: auto;
+			background-color: none;
+		}
 
-    nav {
-      border: 1px solid hsl(var(--crl-white));
-      margin-left: auto;
-      width: auto;
-      border: none;
-    }
-  }
+		nav {
+			border: 1px solid hsl(var(--crl-white));
+			margin-left: auto;
+			width: auto;
+			border: none;
+		}
+	}
 </style>
